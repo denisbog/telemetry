399dc713505f7a52e0f2f135e92f0a6be1c424b8
Denis <denis.boghiu@gmail.com>
2026-01-14 19:46:22 +0200

    use port 80 for the webapp

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ async fn main() {
 
     tokio::task::spawn(listen(samples));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
